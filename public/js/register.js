10ee6f53a1b147123241d5696fb637723f6c81c8
Patry-vll <patryka.vll@gmail.com>
2025-04-11 23:04:31 +0200

    complete validation register(all field)

--- public/js/register.js
+++ public/js/register.js
@@ -1,193 +1,198 @@
+
 function validarNombre(){
     var nombre = document.getElementById("nombre").value;
     let errorNombre = document.getElementById("errorNombre");
     let nombreRegex = /^[a-zA-Z\s]+$/; 
     let nombres = nombre.split(" ");
-    let esValido = true;
+    errorNombre.textContent = "";
     if( !nombre.trim()){
-        errorNombre.textContent = "Debe ingresar un nombre.";
-        esValido = false;
+       errorNombre.textContent = "Debe ingresar un nombre.";
+        return false;
     }
-    if(!isNaN(nombre)){
-        errorNombre.textContent = "No debe ingresar números en el nombre.";
-         esValido = false;
-    }
-    if ( nombre.length < 3 || nombre.length > 15){
-        errorNombre.textContent = "Debe ingresar un nombre entre 3 y 15 caracteres.";
-        esValido = false;
+    if ( nombre.length < 3 || nombre.length > 15 && nombre.trim()){
+        errorNombre.textContent = ("Debe ingresar un nombre entre 3 y 15 caracteres.");
+        return false;
     } 
     if(nombres.length>2 ){
-        errorNombre.textContent = "El nombre no puede contener más de 2 palabras.";
-        console.log(" Se ingresaron más de 2 palabras");
-        esValido = false;
+        errorNombre.textContent = ("El nombre no puede contener más de 2 palabras.");
+        return false;
     } 
     if(!nombreRegex.test(nombre) || !isNaN(nombre)){
-        errorNombre.textContent = "No debe ingresar números en el nombre.";
+        errorNombre.textContent = ("No debe ingresar números en el nombre.");
         console.log(" No debe ingresar números en el nombre");
-        esValido = false;
+        return false;
     } 
-    if(esValido){
-        errorNombre.textContent = ""; 
-        console.log("Nombre válido: " + nombre);
-        return true; // Nombre válido
-     }
+    return true; 
 }
 function validarApellido1(){
     var apellido1 = document.getElementById("apellido1").value;
     let errorApellido1 = document.getElementById("errorApellido1");
     let apellidoRegex = /^[a-zA-Z\s]+$/; 
-    let esValido = true;
     errorApellido1.textContent = ""; 
     if( !apellido1.trim()){
         errorApellido1.textContent = "Debe ingresar el primer apellido.";
         console.log(apellido1.trim());
-        esValido = false;
-    }
-    if(!isNaN(apellido1)){
-        console.log(apellido1);
-        errorApellido1.textContent = "No debe ingresar números en el apellido.";
-         esValido = false;
+        return false;
     }
     if ( apellido1.length < 3 || apellido1.length > 15){
-        console.log(apellido1);
         errorApellido1.textContent = "Debe ingresar un apellido entre 3 y 15 caracteres.";
-        esValido = false;
+        return false;
     } 
     if(!apellidoRegex.test(apellido1) || !isNaN(apellido1)){
         errorApellido1.textContent = "No debe ingresar números en su primer apellido .";
-        esValido = false;
+        return false;
     } 
-    return esValido;
+    return true;
 }
     function validarApellido2(){
         var apellido2 = document.getElementById("apellido2").value;
         let errorApellido2 = document.getElementById("errorApellido2");
         let apellido2Regex = /^[a-zA-Z\s]+$/; 
-       let esValido = true;
        errorApellido2.textContent = ""; 
         if( apellido2.trim() === ""){
-           alert("Debe ingresar el segundo apellido");
            errorApellido2.textContent = "Debe ingresar el segundo apellido.";
-            console.log(apellido2.trim() +" apellido2" + apellido2.length);
-            esValido = false;
+            return false;
         }
         if(!isNaN(apellido2)){
             errorApellido2.textContent = "No debe ingresar números en el segundo apellido.";
-             esValido = false;
+             return false;
         }
         if ( apellido2.length < 3 || apellido2.length > 15){
             errorApellido2.textContent = "Debe ingresar un apellido entre 3 y 15 caracteres.";
-            esValido = false;
+            return false;
         } 
         if(!apellido2Regex.test(apellido2) || !isNaN(apellido2)){
             errorApellido2.textContent = "No debe ingresar números en su segundo apellido .";
-            esValido = false;
+            return false;
         } 
-        return esValido;
+        return true;
     } 
     function validarDireccion(){
         var direccion = document.getElementById("direccion").value;
         let errorDireccion = document.getElementById("errorDireccion");
-        let esValido = true;
+        
         errorDireccion.textContent = ""; 
         if( !direccion.trim()){
             errorDireccion.textContent = "Debe ingresar una dirección válida.";
-            esValido = false;
+           return false;
         }
         if ( direccion.length < 5 || direccion.length > 30){
             errorDireccion.textContent = "Debe ingresar una dirección válida.";
-            esValido = false;
+            return false;
         } 
-       return esValido;
+       return true;
     }
     function validarCodPostal(){
         console.log("validarCodPostal");
         var codPostal = document.getElementById("codPostal").value;
         let errorCodPostal = document.getElementById("errorcodPostal");
         let codPostalRegex = /^[0-9]{5}$/; 
-        let esValido = true;
         errorCodPostal.textContent = ""; 
         if( !codPostal.trim()){
             errorCodPostal.textContent = "Debe ingresar un código postal.";
-            esValido = false;
+            return false;
         }
         if(!codPostalRegex.test(codPostal)){
             errorCodPostal.textContent = "El código postal debe contener 5 dígitos.";
-            esValido = false;
+            return false;
         } 
-        return esValido;
+        return true;
     }
     function validarPais(){
         var pais = document.getElementById("pais").value;
         let errorPais = document.getElementById("errorPais");
         let nombrePais = pais.split(" ");
         let paisRegex = /^[a-zA-Z\s]+$/;
-        let esValido = true;
+        
         errorPais.textContent = ""; 
         if( !pais.trim()){
             errorPais.textContent = "Debe ingresar un país.";
-            esValido = false;
+            return false;
         }
         if( pais.length <5 || pais.length >30 ){
             errorPais.textContent = "Debe ingresar un país entre 5 y 30 caracteres.";
-            esValido = false;
+            return false;
         }
         if(!isNaN(pais)){
             errorPais.textContent = "No debe ingresar números en el país.";
-            esValido = false;
+            return false;
         }
         if(!paisRegex.test(pais) || !isNaN(pais)){
             errorPais.textContent = "No debe ingresar números en el país.";
-            esValido = false;
+            return false;
         }
         if(nombrePais.length >2){
             errorPais.textContent = "El país no puede contener más de 3 palabras.";
-            console.log(" Se ingresaron más de 3 palabras");
-            esValido = false;
+            return false;
         }
             
-    return esValido;
+    return true;
     }
     function validarEmail(){
         var email = document.getElementById("email").value;
         let errorEmail = document.getElementById("errorEmail");
         let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        let esValido = true;
+        
         errorEmail.textContent = ""; 
         if( !email.trim()){
-            errorEmail.textContent += "Debe ingresar un email.";
+            errorEmail.textContent = "Debe ingresar un email.";
             return false;
         }
         if(!emailRegex.test(email)){
-            errorEmail.textContent += "El email no es válido.";
+            errorEmail.textContent = "El email no es válido.";
             return false;
         }
         if(email.length < 12 || email.length > 20){
-            errorEmail.textContent += "Email no válido, máximo 20 carácteres.";
-            esValido = false;
+            errorEmail.textContent = "Email no válido, máximo 20 carácteres.";
+            return false;
         }
-         return esValido;
+         return true;
 
     }
     function validarPassword(){
         var password = document.getElementById("password").value;
         let errorPassword = document.getElementById("errorPassword");
         let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{5,10}$/; 
-        let esValido = true;
+        
         errorPassword.textContent = ""; 
         if( !password.trim()){
-            errorPassword.textContent += "Debe ingresar una contraseña.";
-            esValido = false;
+            console.log("Dentro del primer if");
+          errorPassword.textContent = ( "Debe ingresar una contraseña."); 
+            return false;
         }
-        if(password.length < 5 || password.length > 10){
-            errorPassword.textContent += "La contraseña debe tener entre 5 y 10 caracteres.";
-            esValido = false;
+       if(password.length < 5 || password.length > 10){
+            errorPassword.textContent = "La contraseña debe tener entre 5 y 10 caracteres.";
+            return false;
         } 
         if(!passwordRegex.test(password)){
-            errorPassword.textContent += "La contraseña debe contener al menos una letra mayúscula, una letra minúscula y un número.";
-            esValido = false;
-        }
-        return esValido; 
+            
+            errorPassword.textContent = "La contraseña debe contener al menos una letra mayúscula, una letra minúscula y un número.";
+            return false;
+        }  
+        return true; 
     }
+    function validarFormulario(){
+        let validacionCampos = [validarNombre(), validarApellido1(), validarApellido2(), validarDireccion(),
+                 validarCodPostal(), validarPais(), validarEmail(), validarPassword()];  
+        if(validacionCampos.includes(false)){
+            return false;
+        }  else{return true;}
+
+    }   
+
+    window.addEventListener("DOMContentLoaded", function(){
+        var formulario = document.getElementById("formRegister");
+        formulario.addEventListener("submit", function(event){
+            if(validarFormulario() == false){
+                //detiene el envio del formulario si falla
+                event.preventDefault(); 
+                console.log("Formulario no válido.");
+
+            }else{
+                console.log("Formulario válido.");
+            }
+        });
+
+    });
     
           
